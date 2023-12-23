24a5ef96c5f25966ab73a184ba676621ed5f09b8
etthann <110430934+etthann@users.noreply.github.com>
2023-12-23 01:32:36 -0500

    WHy wont this work

--- src/components/MainScreenAnimation/Framework/Framework.tsx
+++ src/components/MainScreenAnimation/Framework/Framework.tsx
@@ -1,8 +1,8 @@
 import observer from "../Obeserver";
 import React, { useEffect } from "react";
 import "./Framework.css";
-import Expo from '../../../images/MainImages/png/expo.png'
-import ReactLogo from '../../../images/MainImages/png/react.png'
+import Expo from './././images/MainImages/png/expo.png'
+import ReactLogo from './././images/MainImages/png/react.png'
 
 const Frameworks: React.FC = () => {
   useEffect(() => {
