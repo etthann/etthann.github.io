38d2111a828dbad2a5d0cf76a135d4af6960d47a
Ethan Ieong <ethanieong99@gmail.com>
2024-06-30 10:40:49 -0400

    added background

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable jsx-a11y/anchor-has-content */
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import ScrollToPage from "../Scroll";
@@ -32,7 +31,7 @@ export default function Nav() {
         window.removeEventListener("scroll", controlNavbar);
       };
     }
-  }, [lastScrollY]);
+  }, [controlNavbar, lastScrollY]);
   return (
     <nav
       className={`navbar navbar-expand-lg navbar-light bg-dark p-4 navbar-border rounded-bottom ${
