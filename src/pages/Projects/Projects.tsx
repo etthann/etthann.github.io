f42736f1dbc5f268d08de32d769fa14218855e9e
etthann <110430934+etthann@users.noreply.github.com>
2025-10-23 21:49:43 -0400

    Feat: idk man

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -46,7 +46,6 @@ const Projects: React.FC = () => {
           {
             headers: {
               Accept: "application/vnd.github+json",
-              Authorization: `Bearer ${process.env.REACT_APP_GITHUB_PROFILE_TOKEN}`,
               "X-GitHub-Api-Version": "2022-11-28",
             }
           }
